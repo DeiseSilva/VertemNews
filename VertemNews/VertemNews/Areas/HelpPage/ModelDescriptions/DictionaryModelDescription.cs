0057983a423e365aa4577622a0123e4fa369f3c9
Deise Silva <89200897+DeiseSilva@users.noreply.github.com>
2023-06-02 14:47:37 -0300

    Novo projeto

---
+++ VertemNews/VertemNews/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VertemNews.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ VertemNews/VertemNews/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VertemNews.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
